b88d0aab2cf4d18e3b5e21bcc75722f33d337ae1
zxjdzxb <1660154581@qq.com>
2022-12-16 18:09:20 +0800

    deleted dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/catplus/dist/',
+  // base:'/catplus/dist/',
   plugins: [
     vue(),
     vueJsx({
