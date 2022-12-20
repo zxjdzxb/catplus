c769c7c65f062837d9a9daefba2449debb52c1b0
zxjdzxb <1660154581@qq.com>
2022-12-20 21:58:27 +0800

    引入VueRouter@4

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, ref } from "vue";
+import { RouterView } from "vue-router";
 
 export const App = defineComponent({
   setup() {
@@ -7,12 +8,24 @@ export const App = defineComponent({
       refCount.value += 1
     }
     return () => <>
+    <header>
+      <ul>
+        <li>
+        <ul>
+          <li>
+            <router-link to="/">Foo</router-link>
+          </li>
+          <li>
+            <router-link to="/bar">Bar</router-link>
+          </li>
+        </ul> 
+        </li>
+      </ul>
+    </header>
       <div>
-        {refCount.value}
+      <RouterView />
       </div>
-      <div>
-        <button onClick={onClick}>+1</button>
-      </div>
+      <footer>页脚</footer>
     </>
 
   }
