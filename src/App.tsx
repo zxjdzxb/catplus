6ac5d4c7c4bc8c4cd5cf0bac016fead5facd0b9b
zxjdzxb <zhouxingjie@philisense.com>
2022-12-25 13:56:00 +0800

    创建子路由和初始化SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
 import { defineComponent, ref } from "vue";
 import { RouterView } from "vue-router";
+import "./App.scss"
 
 export const App = defineComponent({
   setup() {
-    const refCount = ref(0)
-    const onClick = ()=>{
-      refCount.value += 1
-    }
-    return () => <>
-    <header>
-      <ul>
-        <li>
-        <ul>
-          <li>
-            <router-link to="/">Foo</router-link>
-          </li>
-          <li>
-            <router-link to="/bar">Bar</router-link>
-          </li>
-        </ul> 
-        </li>
-      </ul>
-    </header>
-      <div>
-      <RouterView />
-      </div>
-      <footer>页脚</footer>
-    </>
 
+    return () => (
+
+      <div>
+        <RouterView />
+      </div>
+    )
   }
 })
