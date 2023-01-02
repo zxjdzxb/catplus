fbca5dd609ee20a1ec94e09770ee858bf5278fef
zxjdzxb <1660154581@qq.com>
2023-01-02 20:46:48 +0800

    完成数字输入功能

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -14,7 +14,7 @@ export const InputPad = defineComponent({
     const now = new Date();
     let refDate = ref<Date>(now);
     const refDatePickerVisible = ref(false)
-    const refAmount = ref('')
+    const refAmount = ref('0')
     const buttons = [
       {
         text: '1', onClick: () => { appendText(1) }
@@ -29,13 +29,38 @@ export const InputPad = defineComponent({
       { text: '9', onClick: () => { appendText(9) } },
       { text: '.', onClick: () => { appendText('.') } },
       { text: '0', onClick: () => { appendText(0) } },
-      { text: '清空', onClick: () => { } },
+      { text: '清空', onClick: () => {  refAmount.value = '0'} },
       { text: '提交', onClick: () => { } },
     ];
     const showDatePicker = () => refDatePickerVisible.value = true
     const hideDatePicker = () => refDatePickerVisible.value = false
     const setDate = (date: Date) => { refDate.value = date; hideDatePicker() }
-    const appendText = (n: number | string) => refAmount.value += n.toString()
+    const appendText = (n: number | string) => {
+      const nString = n.toString()
+      const dotIndex = refAmount.value.indexOf('.')
+      if (refAmount.value.length >= 13) {
+        return
+      }
+      if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) {
+        return
+      }
+      if (nString === '.') {
+        if (dotIndex >= 0) { // 已经有小数点了
+          return
+        }
+      } else if (nString === '0') {
+        if (dotIndex === -1) { // 没有小数点
+          if (refAmount.value === '0') { // 没小数点，但是有0
+            return
+          }
+        }
+      } else {
+        if (refAmount.value === '0') {
+          refAmount.value = ''
+        }
+      }
+      refAmount.value += n.toString()
+    }
     return () => <>
       <div class={s.dateAndAmount}>
         <span class={s.date}>
