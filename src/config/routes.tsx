e62d42a6968d35a61035f8d3a55b7dc901abca26
zxjdzxb <1660154581@qq.com>
2023-01-02 11:23:14 +0800

    创建 /items 和 /items/create 路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -9,6 +9,9 @@ import { Third } from "../components/welcome/Third";
 import { ThirdActions } from "../components/welcome/ThirdActions";
 import { Welcome } from "../views/Welcome";
 import {StartPage} from '../views/StartPage';
+import {ItemCreate} from '../components/item/ItemCreate';
+import {ItemList} from '../components/item/ItemList';
+import {ItemPage} from '../views/ItemPage';
 
 export const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/welcome' },
@@ -23,5 +26,12 @@ export const routes: RouteRecordRaw[] = [
       { path: '4', name: "Welcome4", components: { main: Forth, footer: ForthActions }, },
     ]
   },
-  {path:'/start', component: StartPage}
+  { path: '/start', component: StartPage },
+  {
+    path: '/items', component: ItemPage,
+    children: [
+      { path: '', component: ItemList },
+      { path: 'create', component: ItemCreate },
+    ]
+  },
 ]
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,36 +1,43 @@
 import {defineComponent, ref} from 'vue';
-import { Button } from '../shared/Button';
+import {Button} from '../shared/Button';
 import s from './StartPage.module.scss';
 import {FloatButton} from '../shared/FloatButton';
 import {Icon} from '../shared/Icon';
 import {Center} from '../shared/Center';
 import {Navbar} from '../shared/Navbar';
 import {Overlay} from '../shared/Overlay';
+import {RouterLink} from 'vue-router';
+
 export const StartPage = defineComponent({
   setup: (props, context) => {
-    const refOverlayVisible = ref(false)
+    const refOverlayVisible = ref(false);
     const onClickMenu = () => {
-      refOverlayVisible.value = !refOverlayVisible.value
-    }
+      refOverlayVisible.value = !refOverlayVisible.value;
+    };
     return () => (
       <div>
         <Navbar>{
           {
             default: () => '山竹记账',
-            icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
+            icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu}/>
           }
         }</Navbar>
         <Center class={s.pig_wrapper}>
-          <Icon name="pig" class={s.pig} />
+          <Icon name="pig" class={s.pig}/>
         </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button}>开始记账</Button>
+          <RouterLink to="/items/create">
+            <Button class={s.button}>开始记账</Button>
+          </RouterLink>
         </div>
-        <FloatButton iconName='add'/>
+        <RouterLink to="/items/create">
+          <FloatButton iconName="add"/>
+        </RouterLink>
+
         {refOverlayVisible.value &&
-          <Overlay onClose={() => refOverlayVisible.value = false} />
+          <Overlay onClose={() => refOverlayVisible.value = false}/>
         }
       </div>
-    )
+    );
   }
-})
+});
