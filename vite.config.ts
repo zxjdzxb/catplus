8a68786039fe79269d590228ca26f7bd31646de9
zxjdzxb <1660154581@qq.com>
2023-01-03 21:15:18 +0800

    添加 tag 相关页面

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { svgstore } from './src/vite_plugins/svgstore';
 import styleImport, { VantResolve } from 'vite-plugin-style-import';
-import Components from 'unplugin-vue-components/vite';
-import { VantResolver } from 'unplugin-vue-components/resolvers';
 // https://vitejs.dev/config/
 export default defineConfig({
   // base:'/catplus/dist/',
