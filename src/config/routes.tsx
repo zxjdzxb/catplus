dcfbfae6b9b8bd8fe018b902524991bfba675470
zxjdzxb <1660154581@qq.com>
2023-01-06 21:13:46 +0800

    完成 TagCreate 样式

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,13 +1,13 @@
-import { RouteRecordRaw } from "vue-router";
-import { First } from "../components/welcome/First";
-import { FirstActions } from "../components/welcome/FirstActions";
-import { Forth } from "../components/welcome/Forth";
-import { ForthActions } from "../components/welcome/ForthActions";
-import { Second } from "../components/welcome/Second";
-import { SecondActions } from "../components/welcome/SecondActions";
-import { Third } from "../components/welcome/Third";
-import { ThirdActions } from "../components/welcome/ThirdActions";
-import { Welcome } from "../views/Welcome";
+import {RouteRecordRaw} from 'vue-router';
+import {First} from '../components/welcome/First';
+import {FirstActions} from '../components/welcome/FirstActions';
+import {Forth} from '../components/welcome/Forth';
+import {ForthActions} from '../components/welcome/ForthActions';
+import {Second} from '../components/welcome/Second';
+import {SecondActions} from '../components/welcome/SecondActions';
+import {Third} from '../components/welcome/Third';
+import {ThirdActions} from '../components/welcome/ThirdActions';
+import {Welcome} from '../views/Welcome';
 import {StartPage} from '../views/StartPage';
 import {ItemCreate} from '../components/item/ItemCreate';
 import {ItemList} from '../components/item/ItemList';
@@ -17,24 +17,24 @@ import {TagEdit} from '../components/tag/TagEdit';
 import {TagPage} from '../views/TagPage';
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', redirect: '/welcome' },
+  {path: '/', redirect: '/welcome'},
   {
     path: '/welcome',
     component: Welcome,
     children: [
-      { path: '', redirect: '/welcome/1', },
-      { path: '1', name: "Welcome1", components: { main: First, footer: FirstActions }, },
-      { path: '2', name: "Welcome2", components: { main: Second, footer: SecondActions }, },
-      { path: '3', name: "Welcome3", components: { main: Third, footer: ThirdActions }, },
-      { path: '4', name: "Welcome4", components: { main: Forth, footer: ForthActions }, },
+      {path: '', redirect: '/welcome/1',},
+      {path: '1', name: 'Welcome1', components: {main: First, footer: FirstActions},},
+      {path: '2', name: 'Welcome2', components: {main: Second, footer: SecondActions},},
+      {path: '3', name: 'Welcome3', components: {main: Third, footer: ThirdActions},},
+      {path: '4', name: 'Welcome4', components: {main: Forth, footer: ForthActions},},
     ]
   },
-  { path: '/start', component: StartPage },
+  {path: '/start', component: StartPage},
   {
     path: '/items', component: ItemPage,
     children: [
-      { path: '', component: ItemList},
-      { path: 'create', component: ItemCreate },
+      {path: '', component: ItemList},
+      {path: 'create', component: ItemCreate},
     ]
   },
   {
@@ -44,4 +44,4 @@ export const routes: RouteRecordRaw[] = [
       {path: ':id', component: TagEdit}
     ]
   },
-]
+];
