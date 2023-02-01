1d1edb8b6d997502f91c88ecf3d708ed588c98b3
zxjdzxb <1660154581@qq.com>
2023-02-01 21:53:31 +0800

    添加Overlay 打算封装 Form 组件

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,10 +1,11 @@
-import {defineComponent, PropType, reactive, ref} from 'vue';
+import {defineComponent, PropType, reactive, ref, watchEffect} from 'vue';
 import s from './ItemList.module.scss';
 import {Tab, Tabs} from '../../shared/Tabs';
 import {Icon} from '../../shared/Icon';
 import {MainLayout} from '../../layouts/MainLayout';
 import {Time} from '../../shared/time';
 import {ItemSummary} from './ItemSummary';
+import {Overlay} from 'vant';
 export const ItemList = defineComponent({
   setup: (props, context) => {
     const refSelected = ref('本月')
@@ -27,12 +28,18 @@ export const ItemList = defineComponent({
         end: time.lastDayOfYear()
       }
     ]
+    watchEffect(() => {
+      if (refSelected.value === '自定义时间') {
+        refOverlayVisible.value = true
+      }
+    })
+    const refOverlayVisible = ref(false)
     return () => (
       <MainLayout>{
         {
           title: () => '山竹记账',
           icon: () => <Icon name="menu" />,
-          default: () => (
+          default: () => <>
             <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
               <Tab name="本月">
                 <ItemSummary
@@ -55,7 +62,24 @@ export const ItemList = defineComponent({
                   endDate={customTime.end.format()} />
               </Tab>
             </Tabs>
-          )
+            <Overlay show={refOverlayVisible.value} class={s.overlay} >
+              <div class={s.overlay_inner}>
+                <header>
+                  请选择时间
+                </header>
+                <main>
+                  <form>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                  </form>
+                </main>
+              </div>
+            </Overlay>
+         </>
         }
       }</MainLayout>
     )
