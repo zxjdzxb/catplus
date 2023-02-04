2e85ff74506c4a030e20d4b063f2f010b3848c6b
zxjdzxb <1660154581@qq.com>
2023-02-04 13:41:43 +0800

    创建 Charts 组件

--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -1,9 +1,10 @@
 import {computed, defineComponent, PropType, ref, VNode} from 'vue';
-import { EmojiSelect } from './EmojiSelect';
+import {EmojiSelect} from './EmojiSelect';
 import s from './Form.module.scss';
 import {DatetimePicker, Popup} from 'vant';
 import {Time} from './time';
 import {Button} from './Button';
+
 export const Form = defineComponent({
   props: {
     onSubmit: {
@@ -15,9 +16,9 @@ export const Form = defineComponent({
       <form class={s.form} onSubmit={props.onSubmit}>
         {context.slots.default?.()}
       </form>
-    )
+    );
   }
-})
+});
 
 export const FormItem = defineComponent({
   props: {
@@ -27,15 +28,16 @@ export const FormItem = defineComponent({
     modelValue: {
       type: [String, Number]
     },
-    type: String as PropType<'text' | 'emojiSelect' | 'date' | 'validationCode'>,
+    type: String as PropType<'text' | 'emojiSelect' | 'date' | 'validationCode' | 'select'>,
     error: {
       type: String
     },
     placeholder: String,
+    options: Array as PropType<Array<{ value: string, text: string }>>
   },
   emits: ['update:modelValue'],
   setup: (props, context) => {
-    const refDateVisible = ref(false)
+    const refDateVisible = ref(false);
     const content = computed(() => {
       switch (props.type) {
         case 'text':
@@ -43,38 +45,45 @@ export const FormItem = defineComponent({
             placeholder={props.placeholder}
             value={props.modelValue}
             onInput={(e: any) => context.emit('update:modelValue', e.target.value)}
-            class={[s.formItem, s.input]} />
+            class={[s.formItem, s.input]}/>;
         case 'emojiSelect':
           return <EmojiSelect
             modelValue={props.modelValue?.toString()}
             onUpdateModelValue={value => context.emit('update:modelValue', value)}
-            class={[s.formItem, s.emojiList, s.error]} />
+            class={[s.formItem, s.emojiList, s.error]}/>;
         case 'validationCode':
           return <>
             <input class={[s.formItem, s.input, s.validationCodeInput]}
-                   placeholder={props.placeholder} />
+                   placeholder={props.placeholder}/>
             <Button class={[s.formItem, s.button, s.validationCodeButton]}>
               发送验证码
             </Button>
-          </>
+          </>;
+        case 'select':
+          return <select class={[s.formItem, s.select]} value={props.modelValue}
+                         onChange={(e: any) => { context.emit('update:modelValue', e.target.value); }}>
+            {props.options?.map(option =>
+              <option value={option.value}>{option.text}</option>
+            )}
+          </select>;
         case 'date':
           return <>
             <input readonly={true} value={props.modelValue}
                    placeholder={props.placeholder}
-                   onClick={() => { refDateVisible.value = true }}
-                   class={[s.formItem, s.input]} />
-            <Popup position='bottom' v-model:show={refDateVisible.value}>
+                   onClick={() => { refDateVisible.value = true; }}
+                   class={[s.formItem, s.input]}/>
+            <Popup position="bottom" v-model:show={refDateVisible.value}>
               <DatetimePicker value={props.modelValue} type="date" title="选择年月日"
                               onConfirm={(date: Date) => {
-                                context.emit('update:modelValue', new Time(date).format())
-                                refDateVisible.value = false
+                                context.emit('update:modelValue', new Time(date).format());
+                                refDateVisible.value = false;
                               }}
-                              onCancel={() => refDateVisible.value = false} />
-            </Popup></>
+                              onCancel={() => refDateVisible.value = false}/>
+            </Popup></>;
         case undefined:
-          return context.slots.default?.()
+          return context.slots.default?.();
       }
-    })
+    });
     return () => {
       return <div class={s.formRow}>
         <label class={s.formLabel}>
@@ -88,7 +97,7 @@ export const FormItem = defineComponent({
             <span>{props.error ?? '　'}</span>
           </div>
         </label>
-      </div>
-    }
+      </div>;
+    };
   }
-})
+});
