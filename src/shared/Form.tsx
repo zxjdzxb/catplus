7d386fe347a2e9fe180531763bb6b17f632ba180
zxjdzxb <1660154581@qq.com>
2023-02-08 21:13:39 +0800

    封装了 HTTP

--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -4,6 +4,7 @@ import s from './Form.module.scss';
 import {DatetimePicker, Popup} from 'vant';
 import {Time} from './time';
 import {Button} from './Button';
+import {getFriendlyError} from './getFrendlyError';
 
 export const Form = defineComponent({
   props: {
@@ -114,7 +115,7 @@ export const FormItem = defineComponent({
             {content.value}
           </div>
           <div class={s.formItem_errorHint}>
-            <span>{props.error ?? '　'}</span>
+            <span>{props.error ?getFriendlyError(props.error): '　'}</span>
           </div>
         </label>
       </div>;
--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -1,16 +1,17 @@
-import {defineComponent, PropType, reactive, ref} from 'vue';
-import { MainLayout } from '../layouts/MainLayout';
-import { Button } from '../shared/Button';
-import { Form, FormItem } from '../shared/Form';
-import { Icon } from '../shared/Icon';
-import { validate } from '../shared/validate';
+import {defineComponent, reactive, ref} from 'vue';
+import {MainLayout} from '../layouts/MainLayout';
+import {Button} from '../shared/Button';
+import {Form, FormItem} from '../shared/Form';
+import {Icon} from '../shared/Icon';
+import {validate} from '../shared/validate';
 import s from './SignInPage.module.scss';
-import axios from 'axios';
+import {http} from '../shared/Http';
+
 export const SignInPage = defineComponent({
   setup: (props, context) => {
     const refValidationCode = ref<any>()
     const formData = reactive({
-      email: '',
+      email: '1660154581@qq.com',
       code: ''
     })
     const errors = reactive({
@@ -29,11 +30,16 @@ export const SignInPage = defineComponent({
         { key: 'code', type: 'required', message: '必填' },
       ]))
     }
+    const onError = (error: any) => {
+      if (error.response.status === 422) {
+        Object.assign(errors, error.response.data.errors)
+      }
+      throw error
+    }
     const onClickSendValidationCode = async () => {
-      const response = await axios.post('/api/v1/validation_codes', { email: formData.email })
-        .catch(()=>{
-          //失败
-        })
+      const response = await http
+        .post('/validation_codes', { email: formData.email })
+        .catch(onError)
       // 成功
       refValidationCode.value.startCount()
     }
@@ -55,7 +61,7 @@ export const SignInPage = defineComponent({
                 <FormItem label="验证码" type="validationCode"
                           ref={refValidationCode}
                           placeholder='请输入六位数字'
-                          countFrom={10}
+                          countFrom={2}
                           onClick={onClickSendValidationCode}
                           v-model={formData.code} error={errors.code?.[0]} />
                 <FormItem style={{ paddingTop: '96px' }}>
