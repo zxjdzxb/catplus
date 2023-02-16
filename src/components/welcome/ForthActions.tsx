36d3a23e1be4f948cde747727d23d6fd15024d14
zxjdzxb <1660154581@qq.com>
2023-02-16 20:42:59 +0800

    如果看过广告，就跳过

--- src/components/welcome/ForthActions.tsx
+++ src/components/welcome/ForthActions.tsx
@@ -1,9 +1,14 @@
 import s from './welcome.module.scss';
 import { RouterLink } from 'vue-router';
+const onClick = () => {
+  localStorage.setItem('skipFeatures', 'yes')
+}
 export const ForthActions = () => (
   <div class={s.actions}>
     <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-    <RouterLink to="/start" >完成</RouterLink>
+    <span onClick={onClick}>
+      <RouterLink to="/start">完成</RouterLink>
+    </span>
     <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
   </div>
 )
