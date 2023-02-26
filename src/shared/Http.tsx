719d7c5170f44e102c9df13cab13dfd6fa43cd9d
zxjdzxb <1660154581@qq.com>
2023-02-26 19:53:27 +0800

    mock tagEdit

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -1,5 +1,5 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
-import {mockItemCreate, mockSession, mockTagIndex, mockTagShow} from '../mock/mock';
+import axios, {AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios';
+import {mockItemCreate, mockSession, mockTagEdit, mockTagIndex, mockTagShow} from '../mock/mock';
 
 type GetConfig = Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>
 type PostConfig = Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>
@@ -7,81 +7,89 @@ type PatchConfig = Omit<AxiosRequestConfig, 'url' | 'data'>
 type DeleteConfig = Omit<AxiosRequestConfig, 'params'>
 
 export class Http {
-  instance: AxiosInstance
+  instance: AxiosInstance;
+
   constructor(baseURL: string) {
     this.instance = axios.create({
       baseURL
-    })
+    });
   }
+
   get<R = unknown>(url: string, query?: Record<string, JSONValue>, config?: GetConfig) {
-    return this.instance.request<R>({ ...config, url: url, params: query, method: 'get' })
+    return this.instance.request<R>({...config, url: url, params: query, method: 'get'});
   }
+
   post<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PostConfig) {
-    return this.instance.request<R>({ ...config, url, data, method: 'post' })
+    return this.instance.request<R>({...config, url, data, method: 'post'});
   }
+
   patch<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PatchConfig) {
-    return this.instance.request<R>({ ...config, url, data, method: 'patch' })
+    return this.instance.request<R>({...config, url, data, method: 'patch'});
   }
+
   delete<R = unknown>(url: string, query?: Record<string, string>, config?: DeleteConfig) {
-    return this.instance.request<R>({ ...config, url: url, params: query, method: 'delete' })
+    return this.instance.request<R>({...config, url: url, params: query, method: 'delete'});
   }
 }
 
 const mock = (response: AxiosResponse) => {
   if (location.hostname !== 'localhost'
     && location.hostname !== '127.0.0.1'
-    && location.hostname !== '192.168.3.57') { return false }
+    && location.hostname !== '192.168.3.57') { return false; }
   switch (response.config?.params?._mock) {
     case 'tagIndex':
-      [response.status, response.data] = mockTagIndex(response.config)
-      return true
+      [response.status, response.data] = mockTagIndex(response.config);
+      return true;
     case 'session':
-      [response.status, response.data] = mockSession(response.config)
-      return true
+      [response.status, response.data] = mockSession(response.config);
+      return true;
     case 'itemCreate':
-      [response.status, response.data] = mockItemCreate(response.config)
-      return true
+      [response.status, response.data] = mockItemCreate(response.config);
+      return true;
     case 'tagShow':
-      [response.status, response.data] = mockTagShow(response.config)
-      return true
+      [response.status, response.data] = mockTagShow(response.config);
+      return true;
+    case 'tagEdit':
+      [response.status, response.data] = mockTagEdit(response.config);
+      return true;
   }
-  return false
-}
+  return false;
+};
 
-export const http = new Http('/api/v1')
+export const http = new Http('/api/v1');
 
 http.instance.interceptors.request.use(config => {
-  const jwt = localStorage.getItem('jwt')
+  const jwt = localStorage.getItem('jwt');
   if (jwt) {
-    config.headers!.Authorization = `Bearer ${jwt}`
+    config.headers!.Authorization = `Bearer ${jwt}`;
   }
-  return config
-})
+  return config;
+});
 
 http.instance.interceptors.response.use((response) => {
-  mock(response)
+  mock(response);
   if (response.status >= 400) {
-    throw { response }
+    throw {response};
   } else {
-    return response
+    return response;
   }
 }, (error) => {
-  mock(error.response)
+  mock(error.response);
   if (error.response.status >= 400) {
-    throw error
+    throw error;
   } else {
-    return error.response
+    return error.response;
   }
-})
+});
 http.instance.interceptors.response.use(
-  response => { return response },
+  response => { return response; },
   error => {
     if (error.response) {
-      const axiosError = error as AxiosError
+      const axiosError = error as AxiosError;
       if (axiosError.response?.status === 429) {
-        alert('你太频繁了')
+        alert('你太频繁了');
       }
     }
-    throw error
+    throw error;
   }
-)
+);
