89ad69def3a9ce5d5c2db5f1254ea9d04e968341
zxj <1660154581@qq.com>
2023-03-03 16:30:19 +0800

    初步转换折线图数据

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -2,7 +2,7 @@ import axios, {AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse} fro
 import {
   mockItemCreate,
   mockItemIndex,
-  mockItemIndexBalance,
+  mockItemIndexBalance, mockItemSummary,
   mockSession,
   mockTagEdit,
   mockTagIndex,
@@ -65,6 +65,9 @@ const mock = (response: AxiosResponse) => {
       return true
     case 'itemIndexBalance':
       [response.status, response.data] = mockItemIndexBalance(response.config)
+      return true
+    case 'itemSummary':
+      [response.status, response.data] = mockItemSummary(response.config)
       return true
   }
   return false;
