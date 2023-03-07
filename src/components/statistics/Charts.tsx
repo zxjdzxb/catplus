0223e85056ec7cdf69f7c06474dc8ded94ab6f4f
zxjdzxb <1660154581@qq.com>
2023-03-07 21:39:56 +0800

    数据修剪，改补零的补零

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -6,7 +6,8 @@ import {LineChart} from './LineChart';
 import {Bars} from './Bars';
 import {http} from '../../shared/Http';
 
-type Data1Item = {happen_at:string, amount: number}
+
+type Data1Item = { happen_at: string, amount: number }
 type Data1 = Data1Item[]
 export const Charts = defineComponent({
   props: {
@@ -20,36 +21,50 @@ export const Charts = defineComponent({
     }
   },
   setup: (props, context) => {
-    const kind = ref('expenses')
-    const data1 = ref<Data1>([])
-    const betterData1 = computed(()=> {
-      return data1.value.map(item=>
-        [item.happen_at, item.amount] as [string, number]
-      )
-    })
+    const kind = ref('expenses');
+    const data1 = ref<Data1>([]);
+    const betterData1 = computed(() => {
+      if (!props.startDate || !props.endDate) {
+        return [];
+      }
+      const array = [];
+      const n = new Date(props.endDate).getDate() - new Date(props.startDate).getDate() + 1;
+      let j = 0;
+      for (let i = 0; i < n; i++) {
+        const date = new Date(props.startDate + 'T16:00:00.000Z');
+        date.setDate(date.getDate() + i);
+        const dateStr = date.toISOString();
+        if (data1.value[j] && data1.value[j].happen_at === dateStr) {
+          array.push([dateStr, data1.value[j].amount]);
+          j += 1;
+        } else {
+          array.push([dateStr, 0]);
+        }
+      }
+      return array as [string, number][];
 
-    onMounted(async ()=>{
-      const response = await http.get<{groups: Data1, summary: number}>('/items/summary',{
+    });
+
+    onMounted(async () => {
+      const response = await http.get<{ groups: Data1, summary: number }>('/items/summary', {
         happen_after: props.startDate,
         happen_before: props.endDate,
         kind: kind.value,
         _mock: 'itemSummary'
-      })
-      console.log('response.data')
-      console.log(response.data)
-      data1.value = response.data.groups
-    })
+      });
+      data1.value = response.data.groups;
+    });
 
     return () => (
       <div class={s.wrapper}>
-        <FormItem label='类型' type="select" options={[
-          { value: 'expenses', text: '支出' },
-          { value: 'income', text: '收入' }
-        ]} v-model={kind.value} />
+        <FormItem label="类型" type="select" options={[
+          {value: 'expenses', text: '支出'},
+          {value: 'income', text: '收入'}
+        ]} v-model={kind.value}/>
         <LineChart data={betterData1.value}/>
-        <PieChart />
-        <Bars />
+        <PieChart/>
+        <Bars/>
       </div>
-    )
+    );
   }
-})
+});
