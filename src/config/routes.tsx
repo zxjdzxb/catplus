9aeb9fd821c244e85620d9084749bb49db4fdc5c
zxj <1660154581@qq.com>
2023-03-09 11:09:49 +0800

    提示「敬请期待」

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -17,6 +17,7 @@ import {TagEdit} from '../components/tag/TagEdit';
 import {TagPage} from '../views/TagPage';
 import {SignInPage} from '../views/SignInPage';
 import {StatisticsPage} from '../views/StatisticsPage';
+import {ComingSoon} from '../shared/ComingSoon';
 
 
 export const routes: RouteRecordRaw[] = [
@@ -55,5 +56,9 @@ export const routes: RouteRecordRaw[] = [
   },
   {
     path: '/statistics', component: StatisticsPage
+  },{
+    path: '/export', component: ComingSoon
+  },{
+    path: '/notify', component: ComingSoon
   }
 ];
