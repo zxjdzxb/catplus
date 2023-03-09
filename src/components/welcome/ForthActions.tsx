4b2fb54025174c3c6c249699b7ec972913d3fcf8
zxj <1660154581@qq.com>
2023-03-09 11:16:30 +0800

    合并 /start 和 /items

--- src/components/welcome/ForthActions.tsx
+++ src/components/welcome/ForthActions.tsx
@@ -7,7 +7,7 @@ export const ForthActions = () => (
   <div class={s.actions}>
     <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
     <span onClick={onClick}>
-      <RouterLink to="/start">完成</RouterLink>
+      <RouterLink to="/items">完成</RouterLink>
     </span>
     <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
   </div>
