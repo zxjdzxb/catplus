95d2c660ae2b89794562023aad16e9c9aa0591d0
zxj <1660154581@qq.com>
2023-03-10 16:06:05 +0800

    tags_id 改为 tag_ids

--- src/env.d.ts
+++ src/env.d.ts
@@ -14,7 +14,7 @@ type Tag = {
   user_id: number,
   name: string,
   sign: string,
-  kind: expenses | income
+  kind: 'expenses' | 'income';
 }
 type Resources<T = any> = {
   resources: T[]
@@ -35,12 +35,13 @@ type Item = {
   id: number
   user_id: number
   amount: number
-  tags_id: number[]
+  tag_ids: number[]
   tags?: Tag[]
   happen_at: string
-  kind: expenses | income
+  kind: 'expenses' | 'income';
 }
 type User = {
   id: number;
   email: string;
 }
+type FormErrors<T> = {[K in keyof typeof T]: string[]}
