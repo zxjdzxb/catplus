8ae261896f38831e22979cad68cd19581f691862
zxjdzxb <1660154581@qq.com>
2023-03-11 22:57:04 +0800

    整理后退功能

--- src/shared/getFrendlyError.tsx
+++ src/shared/getFrendlyError.tsx
@@ -2,6 +2,6 @@ const map: Record<string, string> = {
   'is invalid': '格式不正确'
 }
 export const getFriendlyError = (error: string) => {
-  console.log(error)
+  //console.log(error)
   return map[error] || error
 }
