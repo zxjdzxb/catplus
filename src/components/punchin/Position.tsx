d0d2d116dcb307df19061b9f33de57ab13093e46
zxjdzxb <1660154581@qq.com>
2023-04-10 16:24:32 +0800

    地图框架

---
+++ src/components/punchin/Position.tsx
@@ -0,0 +1,107 @@
+import {defineComponent, onMounted, PropType, reactive, ref} from 'vue';
+import s from './Position.module.scss';
+import AMapLoader from '@amap/amap-jsapi-loader';
+
+type Position = {
+  path: string[]
+  current_position: string[]
+};
+export const Position = defineComponent({
+  props: {
+    name: {
+      type: String as PropType<string>
+    }
+  },
+  setup: (props, context) => {
+    const state = reactive<Position>({
+      path: [],
+      current_position: [],
+    });
+    const circle = ref(null);
+
+    function initMap() {
+      AMapLoader.load({
+        key: '641de6dcc99dbf8afb4725649c15949f', // 申请好的Web端开发者Key，首次调用 load 时必填
+        version: '2.0', // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+      })
+        .then((AMap) => {
+          const map = new AMap.Map('container', {
+            //设置地图容器id
+            viewMode: '2D', //是否为3D地图模式
+            zoom: 16, //初始化地图级别
+            center: [116.365873,39.985891], //初始化地图中心点位置
+          });
+          //添加插件
+          AMap.plugin(['AMap.ToolBar', 'AMap.Scale', 'AMap.HawkEye'], function () {
+            //异步同时加载多个插件
+            map.addControl(new AMap.HawkEye()); //显示缩略图
+            map.addControl(new AMap.Scale()); //显示当前地图中心的比例尺
+          });
+          AMap.plugin('AMap.Geolocation', function() {
+            var geolocation = new AMap.Geolocation({
+              enableHighAccuracy: true,//是否使用高精度定位，默认:true
+              timeout: 10000,          //超过10秒后停止定位，默认：5s
+              buttonPosition:'RB',    //定位按钮的停靠位置
+              buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+              zoomToAccuracy: true,   //定位成功后是否自动调整地图视野到定位点
+
+            });
+            map.addControl(geolocation);
+
+          });
+          // 单击
+          map.on('click', (e: any) => {
+            // console.log(e);
+            state.current_position = [e.lnglat.KL, e.lnglat.kT];
+            //@ts-ignore
+            state.path.push([e.lnglat.KL, e.lnglat.kT]);
+            addMarker();
+            addPolyLine();
+            // 地图按照适合展示图层内数据的缩放等级展示
+            // map.setFitView();
+          });
+          function addMarker() {
+            const marker = new AMap.Marker({
+              icon: '//a.amap.com/jsapi_demos/static/demo-center/icons/poi-marker-default.png',
+              position: state.current_position,
+              offset: new AMap.Pixel(-26, -54),
+            });
+            marker.setMap(map);
+          }
+
+          // 折线
+          function addPolyLine() {
+            const polyline = new AMap.Polyline({
+              path: state.path,
+              isOutline: true,
+              outlineColor: '#ffeeff',
+              borderWeight: 1,
+              strokeColor: '#3366FF',
+              strokeOpacity: 0.6,
+              strokeWeight: 5,
+              // 折线样式还支持 'dashed'
+              strokeStyle: 'solid',
+              // strokeStyle是dashed时有效
+              // strokeDasharray: [10, 5],
+              lineJoin: 'round',
+              lineCap: 'round',
+              zIndex: 50,
+            });
+            map.add([polyline]);
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+
+    onMounted(() => {
+      initMap();
+    });
+    return () => (
+      <div class={s.wrapper}>
+        <div id="container" class={s.container}></div>
+      </div>
+    );
+  }
+});
