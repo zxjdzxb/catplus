6b6fb7b19eaf97cbc169bab9f27b4add92e295c0
zxjdzxb <1660154581@qq.com>
2023-04-12 11:37:36 +0800

    格式化

--- src/vite_plugins/svgstore.js
+++ src/vite_plugins/svgstore.js
@@ -31,23 +31,23 @@ export const svgstore = (options = {}) => {
           ]
         });
         return `const div = document.createElement('div')
-div.innerHTML = \`${code}\`
-const svg = div.getElementsByTagName('svg')[0]
-if (svg) {
-  svg.style.position = 'absolute'
-  svg.style.width = 0
-  svg.style.height = 0
-  svg.style.overflow = 'hidden'
-  svg.setAttribute("aria-hidden", "true")
-}
-// listen dom ready event
-document.addEventListener('DOMContentLoaded', () => {
-  if (document.body.firstChild) {
-    document.body.insertBefore(div, document.body.firstChild)
-  } else {
-    document.body.appendChild(div)
-  }
-})`;
+                  div.innerHTML = \`${code}\`
+                  const svg = div.getElementsByTagName('svg')[0]
+                    if (svg) {
+                      svg.style.position = 'absolute'
+                      svg.style.width = 0
+                      svg.style.height = 0
+                      svg.style.overflow = 'hidden'
+                      svg.setAttribute("aria-hidden", "true")
+                    }
+                    // listen dom ready event
+                    document.addEventListener('DOMContentLoaded', () => {
+                      if (document.body.firstChild) {
+                        document.body.insertBefore(div, document.body.firstChild)
+                      } else {
+                        document.body.appendChild(div)
+                      }
+                    })`;
       }
     }
   };
