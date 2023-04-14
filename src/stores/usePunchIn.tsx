02bdfdca99d072e24cd9b192cbbbeec227d9bbf5
zxjdzxb <1660154581@qq.com>
2023-04-14 16:19:28 +0800

    setTimeout 模拟 setInterval

--- src/stores/usePunchIn.tsx
+++ src/stores/usePunchIn.tsx
@@ -1,28 +1,52 @@
 //import store from "@/store"
 import {defineStore} from 'pinia';
-import {ref} from 'vue';
+import {computed, ref} from 'vue';
 
 export const usePunchIn = defineStore('counter', () => {
   const gowork = ref<number>();
   const gohometime = ref<number>();
-  const time = ref<number>();
   const hour = ref<number>();
   const minute = ref<number>();
   const second = ref<number>();
-  let timer:any;
-  function countdown() {
-    clearInterval(timer);
-      timer = setInterval(() => {
+  let timer: any = null;
+  //function countdown() {
+  //  clearInterval(timer);
+  //    timer = setInterval(() => {
+  //    if (gowork.value && gohometime.value) {
+  //      time.value = gohometime.value - gowork.value;
+  //      hour.value = Math.floor(time.value / 1000 / 60 / 60 % 24);
+  //      minute.value = Math.floor(time.value / 1000 / 60 % 60);
+  //      second.value = Math.floor(time.value / 1000 % 60);
+  //      //倒计时
+  //      gowork.value += 1000;
+  //    }
+  //  }, 1000);
+  //}
+  const time = computed({
+    get: () => {
       if (gowork.value && gohometime.value) {
-        time.value = gohometime.value - gowork.value;
-        hour.value = Math.floor(time.value / 1000 / 60 / 60 % 24);
-        minute.value = Math.floor(time.value / 1000 / 60 % 60);
-        second.value = Math.floor(time.value / 1000 % 60);
-        //倒计时
-        gowork.value += 1000;
+        return (gohometime.value - gowork.value);
+      } else {
+        return 0;
       }
-    }, 1000);
+    },
+    set: (val) => {
+      return val;
+    }
+  });
 
+  function countdown() {
+    clearTimeout(timer);
+    if (!gowork.value) { return;}
+    hour.value = Math.floor(time.value / 1000 / 60 / 60 % 24);
+    minute.value = Math.floor(time.value / 1000 / 60 % 60);
+    second.value = Math.floor(time.value / 1000 % 60);
+    gowork.value += 1000;
+    timer = setTimeout(() => {
+      countdown();
+
+    }, 1000);
   }
-  return { gowork, gohometime, time, hour, minute, second , countdown}
-})
+
+  return {gowork, gohometime, time, hour, minute, second, countdown};
+});
